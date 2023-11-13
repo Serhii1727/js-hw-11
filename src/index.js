f60854002e1855b92b2302268703373e20ebbc37
Serhii1727 <kravchenko087@ukr.net>
2023-11-13 10:46:31 -0500

    add render SearchQuery

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
 import axios from "axios";
 import Notiflix from "notiflix";
+import refs from "./js/refs";
+import { makeFetchPixabay } from "./js/api-services";
 
-console.log("Hello")
+function createSearchQuery() {
+    return refs.form[0].value;
+};
+
+function makeRenderMarkup(searchImage) {
+    makeFetchPixabay(searchImage).then(({ hits }) => {
+        
+        const markup = hits.map(({ downloads, likes, views, comments, previewURL, largeImageURL }) => {
+            return `<img src=${previewURL} alt=${searchImage}>
+                    <ul>
+                        <li>Likes ${likes}</li>
+                        <li>Views ${views}</li>
+                        <li>Comments ${comments}</li>
+                        <li>Downloads ${downloads}</li>
+                    </ul>`
+        }).join('');
+
+        refs.divGallery.insertAdjacentHTML('beforeend', markup);
+    });
+};
+    
+refs.form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const searchImage = createSearchQuery();
+    makeRenderMarkup(searchImage);
+});
--- src/js/api-services.js
+++ src/js/api-services.js
@@ -1,1 +1,6 @@
-const KEY_PIXABAY = "25836176-b4d66cb7105f8e07a6b55e563";   
+const KEY_PIXABAY = "25836176-b4d66cb7105f8e07a6b55e563";
+const BASE_URL = 'https://pixabay.com/api/';
+
+export function makeFetchPixabay(searchImage) {
+    return fetch(`${BASE_URL}?key=${KEY_PIXABAY}&q=${searchImage}&image_type=photo&orientation="horizontal"&safesearch=true"`).then((response) => {return response.json()})
+};
